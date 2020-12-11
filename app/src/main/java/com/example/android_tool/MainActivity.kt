42d12a2717d6aded0791d3407e654899f57b2565
zzy-33 <jordan@ruilisi.co>
2020-12-11 16:23:21 +0800

    add demo

--- app/src/main/java/com/example/android_tool/MainActivity.kt
+++ app/src/main/java/com/example/android_tool/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.android_tool
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.component.AngryDialog
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun dialog(view: View) {
+        AngryDialog.Builder(this)
+            .create()
+            .show()
     }
 }
